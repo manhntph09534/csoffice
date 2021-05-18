216af7ea606a9659d1613ac2a13c695688e3b90e
tranquocbao123 <baotqph09487@fpt.edu.vn>
2021-05-18 10:32:56 +0700

    Update CustomerService.java

--- src/main/java/com/example/cs_office/Service/CustomerService.java
+++ src/main/java/com/example/cs_office/Service/CustomerService.java
@@ -5,6 +5,7 @@ import com.example.cs_office.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,9 +18,15 @@ public class CustomerService {
 
         this.customerRepository = customerRepository;
     }
+
     public List<Customer> getCustomer() {
 
         return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getById(int userId) {
+        Optional<Customer> customer = customerRepository.findById(userId);
+        return customer;
     }
 
     public void addNewCustomer(Customer customer) {
@@ -39,4 +46,20 @@ public class CustomerService {
         customerRepository.deleteById(customerId);
     }
 
+    @Transactional
+    public void updateCustomer(int userId, String fullname, String email, String avatar, String password) {
+        Customer customer = customerRepository.findById(userId).orElseThrow(() -> new IllegalStateException("user with id" + userId + "does not exists"));
+
+//        if(fullname != null && fullname.length() > 0 && !Objects.equals(user.getFullname(),fullname)){
+//            customer.setFullname(fullname);
+//        }
+//        if(email != null && email.length() > 0 && !Objects.equals(user.getEmail(),email)){
+//            user.setEmail(email);
+//        }
+//        if(avatar != null && avatar.length() > 0 && !Objects.equals(user.getAvatar(),avatar)){
+//            user.setAvatar(avatar);
+//        }
+//        if(password != null && password.length() > 0 && !Objects.equals(user.getPassword(),password)){
+//            user.setPassword(password);
+    }
 }
