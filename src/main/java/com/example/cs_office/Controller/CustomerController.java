a3f2d42e9473565601c5dff3031a6ba33939af45
tranquocbao123 <baotqph09487@fpt.edu.vn>
2021-05-18 10:32:52 +0700

    Update CustomerController.java

--- src/main/java/com/example/cs_office/Controller/CustomerController.java
+++ src/main/java/com/example/cs_office/Controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customer")
@@ -26,4 +27,27 @@ public class CustomerController {
     public void insertCustomer(@RequestBody Customer customer) {
         customerService.addNewCustomer(customer);
     }
+
+    @GetMapping(path = "{customerId}")
+    public Optional<Customer> getById(
+            @PathVariable("customerId") int customerId) {
+        return customerService.getById(customerId);
+    }
+
+    @DeleteMapping(path = "{customerId}")
+    public void deleteCustomer(
+            @PathVariable("customerId") int customerId) {
+        customerService.deleteCustomer(customerId);
+    }
+
+    @PutMapping(path = "{userId}")
+    public void updateUser(
+            @PathVariable("userId") int userId,
+            @RequestParam(required = false) String fullname,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String avatar,
+            @RequestParam(required = false) String password) {
+        customerService.updateCustomer(userId, fullname,email,avatar,password);
+    }
+
 }
