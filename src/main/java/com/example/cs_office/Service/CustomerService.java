233455406fc8486ee442fdab26593491c601bc2a
tranquocbao123 <baotqph09487@fpt.edu.vn>
2021-05-18 09:52:09 +0700

    Update CustomerService.java

--- src/main/java/com/example/cs_office/Service/CustomerService.java
+++ src/main/java/com/example/cs_office/Service/CustomerService.java
@@ -1,4 +1,42 @@
 package com.example.cs_office.Service;
 
+import com.example.cs_office.Model.Customer;
+import com.example.cs_office.Repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+
+        this.customerRepository = customerRepository;
+    }
+    public List<Customer> getCustomer() {
+
+        return customerRepository.findAll();
+    }
+
+    public void addNewCustomer(Customer customer) {
+        Optional<Customer> customerOptional =
+                customerRepository.findCustomerById(customer.getId());
+        if (customerOptional.isPresent()) {
+            throw new IllegalStateException("Id taken");
+        }
+        customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(int customerId) {
+        boolean exists = customerRepository.existsById(customerId);
+        if (!exists) {
+            throw new IllegalStateException("customer with id " + customerId + " does not exists");
+        }
+        customerRepository.deleteById(customerId);
+    }
+
 }
