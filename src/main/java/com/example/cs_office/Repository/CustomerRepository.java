486769cfbc6bcc7ca22b80c12556eef465e8c007
tranquocbao123 <baotqph09487@fpt.edu.vn>
2021-05-18 09:52:06 +0700

    Update CustomerRepository.java

--- src/main/java/com/example/cs_office/Repository/CustomerRepository.java
+++ src/main/java/com/example/cs_office/Repository/CustomerRepository.java
@@ -1,4 +1,15 @@
 package com.example.cs_office.Repository;
 
-public class CustomerRepository {
+import com.example.cs_office.Model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    @Query("select s from Customer s where s.id = ?1")
+    Optional<Customer> findCustomerById(Integer integer);
+
 }
