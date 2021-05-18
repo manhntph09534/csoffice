5626d08e43d932793257bcc3e73cf493b7a50c45
tranquocbao123 <baotqph09487@fpt.edu.vn>
2021-05-18 09:52:03 +0700

    Update CustomerController.java

--- src/main/java/com/example/cs_office/Controller/CustomerController.java
+++ src/main/java/com/example/cs_office/Controller/CustomerController.java
@@ -1,4 +1,29 @@
 package com.example.cs_office.Controller;
 
+import com.example.cs_office.Model.Customer;
+import com.example.cs_office.Service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("")
+    public List<Customer> getCustomer() {
+        return customerService.getCustomer();
+    }
+
+    @PostMapping
+    public void insertCustomer(@RequestBody Customer customer) {
+        customerService.addNewCustomer(customer);
+    }
 }
